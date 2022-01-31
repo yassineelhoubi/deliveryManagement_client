a5b36016f527919b968225111b348f1a153b1877
yassineelhoubi <elhoubiyassine@gmail.com>
2022-01-31 13:20:44 +0100

    Add a router and create a folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css';
-
-function App() {
+import { Routes, Route } from "react-router-dom";
+import { FC } from "react";
+import Login from "./pages/Login"
+const App: FC = () => {
   return (
-    <div className="App">
-     myapp
-    </div>
+    <Routes>
+
+      <Route path="/" >
+
+        <Route index element={<Login />} />
+        <Route path="dashboard" >
+
+        </Route>
+      </Route>
+
+
+      {/* <Route path="/admin">
+          <Route path="/login" />
+        </Route> */}
+    </Routes>
+
+
   );
 }
 
