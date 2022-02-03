788e9258e0f8cd0e0c451d8a9e00238ecf7faa53
yassineelhoubi <elhoubiyassine@gmail.com>
2022-02-03 22:28:02 +0100

    Persist user data in the store

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit"
 import usersReducer from "./features/auth/userSlice"
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, usersReducer)
 export const store = configureStore({
     reducer: {
-        user: usersReducer,
-
+        user: persistedReducer,
     }
 })
 
--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,18 @@ import {
   BrowserRouter,
 } from "react-router-dom";
 import { Provider } from "react-redux"
-import { store } from "./Redux/store"
+import { store } from './Redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+let persistor = persistStore(store)
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-
-      <App />
-    </Provider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
